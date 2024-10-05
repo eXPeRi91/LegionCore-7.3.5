bfa280d3d4d78bd62d676169fc1467ddb058f79c
SoulSeekkor <soulseekkor@live.com>
2024-10-05 11:30:47 -0500

    Fixed existing teleport for 'Sorrowmurk' and added missing teleport for 'Bogpaddle'.

--- sql/updates/world/0021_fix_teleports.sql
+++ sql/updates/world/0021_fix_teleports.sql
@@ -1,5 +1,11 @@
 -- Fix existing teleport locations
-DELETE FROM `game_tele` WHERE `name` IN ('CampNarache', 'Valormok');
+DELETE FROM `game_tele` WHERE `name` IN ('CampNarache', 'Valormok', 'Sorrowmurk');
 INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES
 (188, -2895.67, -247.76, 59.809, 4.0043, 1, 'CampNarache'),
-(1280, 3006.65, -4151.15, 101.810, 2.5073, 1, 'Valormok');
+(1280, 3006.65, -4151.15, 101.810, 2.5073, 1, 'Valormok'),
+(899, -10204.53, -4182.26, 22.356, 4.3773, 0, 'Sorrowmurk');
+
+-- Insert missing teleport locations (do not include id column)
+DELETE FROM `game_tele` WHERE `name` IN ('Bogpaddle');
+INSERT INTO `game_tele` (`position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES
+(-9759.19, -3939.73, 21.652, 1.4289, 1, 'Bogpaddle');
