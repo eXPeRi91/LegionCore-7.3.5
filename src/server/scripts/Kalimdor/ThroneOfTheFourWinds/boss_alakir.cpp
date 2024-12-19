84acc630e4d012c94b621427476d46f4696cd165
SoulSeekkor <soulseekkor@live.com>
2024-12-18 19:26:26 -0600

    Fixed some really screwed up code maybe, not sure what the original logic was supposed to be because of horrible indentation.

--- src/server/scripts/Kalimdor/ThroneOfTheFourWinds/boss_alakir.cpp
+++ src/server/scripts/Kalimdor/ThroneOfTheFourWinds/boss_alakir.cpp
@@ -475,13 +475,13 @@ public:
             std::list<Creature*> tornados;
             GetCreatureListWithEntryInGrid(tornados, me, me->GetEntry(), 40.0f);
 
-            if (!tornados.empty() && tornados.size() < 4)
+            //if (!tornados.empty() && tornados.size() < 4)
                 //me->SummonCreature(me->GetEntry(),me->GetPositionX() - (cos(me->GetOrientation())*2),me->GetPositionY() - (sin(me->GetOrientation())*2),me->GetPositionZ());
 
-                    if (me->GetEntry() == 48854)
-                        MoveSide = true; // west
-                    else
-                        MoveSide = false; // east
+            if (me->GetEntry() == 48854)
+                MoveSide = true; // west
+            else
+                MoveSide = false; // east
         }
 
         void UpdateAI(uint32 diff)
