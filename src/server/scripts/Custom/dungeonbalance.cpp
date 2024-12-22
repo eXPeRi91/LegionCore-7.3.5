c4f1983e40ade04b4091153e44fab751407af776
SoulSeekkor <soulseekkor@live.com>
2024-12-22 13:26:41 -0600

    Updated to ignore scaling for garrison maps.

--- src/server/scripts/Custom/dungeonbalance.cpp
+++ src/server/scripts/Custom/dungeonbalance.cpp
@@ -139,7 +139,7 @@ public:
 
     uint32 _Modifier_DealDamage(Unit* target, Unit* attacker, uint32 damage)
     {
-        if (!enabled || !attacker || !attacker->IsInWorld() || !(attacker->GetMap()->IsDungeon() || attacker->GetMap()->IsRaidOrHeroicDungeon()))
+        if (!enabled || !attacker || !attacker->IsInWorld() || !(attacker->GetMap()->IsDungeon() || attacker->GetMap()->IsRaidOrHeroicDungeon()) || IsGarrisonMap(attacker->GetMap()))
             return damage;
 
         int8 maxPlayerCount = attacker->GetMap()->GetMapMaxPlayers();
@@ -228,36 +228,37 @@ public:
         }
     }
 
-    bool IsGarrisonMap(Map* map)
-    {
-        bool garrison = false;
-
-        switch (map->GetId())
-        {
-            case 1158:  // Alliance Lvl 1
-            case 1331:  // Alliance Lvl 2
-            case 1159:  // Alliance Lvl 3
-            case 1473:  // Alliance Shipyard
-            case 1482:  // Alliance Shipyard - Submarine
-            case 1483:  // Alliance Shipyard - Destroyer
-            case 1485:  // Alliance Shipyard - Battleship
-            case 1496:  // Alliance Shipyard - Carrier
-            case 1152:  // Horde Lvl 1
-            case 1330:  // Horde Lvl 2
-            case 1153:  // Horde Lvl 3
-            case 1474:  // Horde Shipyard
-            case 1486:  // Horde Shipyard - Carrier
-            case 1487:  // Horde Shipyard - Submarine
-            case 1488:  // Horde Shipyard - Destroyer
-            case 1497:  // Horde Shipyard - Battleship
-                garrison = true;
-                break;
-            default: break;
-        }
-
-        return garrison;
-    }
 };
+
+bool IsGarrisonMap(Map* map)
+{
+    bool garrison = false;
+
+    switch (map->GetId())
+    {
+    case 1158:  // Alliance Lvl 1
+    case 1331:  // Alliance Lvl 2
+    case 1159:  // Alliance Lvl 3
+    case 1473:  // Alliance Shipyard
+    case 1482:  // Alliance Shipyard - Submarine
+    case 1483:  // Alliance Shipyard - Destroyer
+    case 1485:  // Alliance Shipyard - Battleship
+    case 1496:  // Alliance Shipyard - Carrier
+    case 1152:  // Horde Lvl 1
+    case 1330:  // Horde Lvl 2
+    case 1153:  // Horde Lvl 3
+    case 1474:  // Horde Shipyard
+    case 1486:  // Horde Shipyard - Carrier
+    case 1487:  // Horde Shipyard - Submarine
+    case 1488:  // Horde Shipyard - Destroyer
+    case 1497:  // Horde Shipyard - Battleship
+        garrison = true;
+        break;
+    default: break;
+    }
+
+    return garrison;
+}
 
 void AddSC_DungeonBalance()
 {
